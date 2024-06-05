dedf96c95cced667165f4ea2b3ebcedbb7644f06
DianaC23 <dianacarolina2ne1@gmail.com>
2024-06-05 11:52:24 -0500

    arregle el problema de agregar campers a areas y del contador

--- asignar_campers_a_areas.py
+++ asignar_campers_a_areas.py
@@ -1,28 +1,43 @@
-import camper_in_area
+import manejo_archivos
 def camper_a_areas():
     while True:
-        camperarea = input("Ingrese el ID del estudiante")
-        puesto = camper_in_area.guardar_el_camper()
-        if camperarea in puesto["campers"]:
-            if puesto["campers"][camperarea]["state"] == "aprobado":
-                elegir = int(input("Elija el area de entrenamiento\n (1)para sputnik\n (2)para apolo\n (3)para artemis: "))
-                if elegir == 1:
-                    puesto["campers"][(camperarea)]["AREAS"] = "sputnik"
-                    camper_in_area.guardar_el_camper(puesto[""])
-                    print("El camper ha sido agregado exitosamente")
-                elif elegir == 2:
-                    puesto["campers"][(camperarea)]["AREAS"] = "apolo"
-                    
-                    print("El camper ha sido agregado exitosamente")
-                elif elegir == 3:
-                    puesto["campers"][(camperarea)]["AREAS"] = "artemis"
+        datos = manejo_archivos.cargar_datos("datos.json")
+        id_estudiante = input("Ingrese el ID del estudiante")
 
-                    print("El camper ha sido agregado exitosamente")
-                else:
-                    print("error al tratar de ingresar el trabajo")
+        if id_estudiante not in datos["campers"]:
+            print ("El estudiante no se encuentra registrado")
+            return
+
+        if datos["campers"][id_estudiante]["state"] != "Aprobado":
+            print("El estudiante debe estar aprobado para poder agregarse a un area")
+            return
+
+        elegir = int(input("Elija el area de entrenamiento\n (1)para sputnik\n (2)para apolo\n (3)para artemis: "))
+        #Profesor use este que esta lo deje en cero para probar que no se puede llenar
+        if elegir == 1:
+            #Probar este que esta en cero para probar que no se puede llenar
+            if datos["AREAS"]["aREA1"]["cantidad"] == 0:
+                print("esta lleno no puedes meter mas")
             else:
-                print("El camper no puede entrar a un ara de entrnamiento porque no esta inscrito")
+                datos["AREAS"]["aREA1"]["GUARDAR_CAMPER"].append(id_estudiante)
+                datos["AREAS"]["aREA1"]["cantidad"] -=1
 
+                print("El camper ha sido agregado exitosamente")
+        elif elegir == 2:
+            if datos["AREAS"]["aREA1"]["cantidad"] == 0:
+                print("esta lleno no puedes meter mas")
+            else:
+                datos["AREAS"]["aREA2"]["GUARDAR_CAMPER"].append(id_estudiante)
+                datos["AREAS"]["aREA1"]["cantidad"] -=1
+            print("El camper ha sido agregado exitosamente")
+        elif elegir == 3:
+            if datos["AREAS"]["aREA1"]["cantidad"] == 0:
+                print("esta lleno no puedes meter mas")
+            else:
+                datos["AREAS"]["aREA3"]["GUARDAR_CAMPER"].append(id_estudiante)
+                datos["AREAS"]["aREA1"]["cantidad"] -=1
+            print("El camper ha sido agregado exitosamente")
         else:
-            print("El camper no se encuentra en la base de datos")
-            break
+            print("error al tratar de ingresar el trabajo")
+        
+        manejo_archivos.guardar_datos("datos.json",datos)
---
+++ manejo_archivos.py
@@ -0,0 +1,17 @@
+import json
+#Funcion de cargar datos
+def cargar_datos(nombre_archivo):
+    try:
+        with open(nombre_archivo,"r", encoding="utf-8") as file:
+            print("datos cargados correctamente")
+            return json.load(file)
+    except Exception :
+        print("error al cargar los datos")
+#funcion de guardar datos
+def guardar_datos(nombre_archivo,data):
+    try:
+        with open(nombre_archivo,"w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4,ensure_ascii=False)
+            print("guardo correctamente")
+    except Exception:
+        print("error al guardar los datos")
